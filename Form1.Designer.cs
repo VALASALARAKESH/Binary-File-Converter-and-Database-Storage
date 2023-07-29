fbaf533cbe4a99cfbf49258c584ef6a28c6ec41f
VALASALARAKESH <valasalarakesh1254@gmail.com>
2023-07-29 12:34:24 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Convert_To_Binary_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.dgvDocuments = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.txtExtension = new System.Windows.Forms.TextBox();
+            this.txtFileSize = new System.Windows.Forms.TextBox();
+            this.txtContentType = new System.Windows.Forms.TextBox();
+            this.txtContentID = new System.Windows.Forms.TextBox();
+            this.txtCategoryID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(643, 44);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = " Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(724, 43);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = " Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(151, 86);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(486, 22);
+            this.txtFilePath.TabIndex = 2;
+            this.txtFilePath.TextChanged += new System.EventHandler(this.txtFilePath_TextChanged);
+            // 
+            // dgvDocuments
+            // 
+            this.dgvDocuments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocuments.Location = new System.Drawing.Point(45, 385);
+            this.dgvDocuments.Name = "dgvDocuments";
+            this.dgvDocuments.RowHeadersWidth = 51;
+            this.dgvDocuments.RowTemplate.Height = 24;
+            this.dgvDocuments.Size = new System.Drawing.Size(652, 235);
+            this.dgvDocuments.TabIndex = 3;
+            this.dgvDocuments.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDocuments_CellContentClick);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(44, 343);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(150, 44);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(487, 22);
+            this.txtFileName.TabIndex = 5;
+            // 
+            // txtExtension
+            // 
+            this.txtExtension.Location = new System.Drawing.Point(151, 127);
+            this.txtExtension.Name = "txtExtension";
+            this.txtExtension.Size = new System.Drawing.Size(486, 22);
+            this.txtExtension.TabIndex = 6;
+            // 
+            // txtFileSize
+            // 
+            this.txtFileSize.Location = new System.Drawing.Point(151, 169);
+            this.txtFileSize.Name = "txtFileSize";
+            this.txtFileSize.Size = new System.Drawing.Size(486, 22);
+            this.txtFileSize.TabIndex = 7;
+            // 
+            // txtContentType
+            // 
+            this.txtContentType.Location = new System.Drawing.Point(173, 209);
+            this.txtContentType.Name = "txtContentType";
+            this.txtContentType.Size = new System.Drawing.Size(464, 22);
+            this.txtContentType.TabIndex = 8;
+            this.txtContentType.TextChanged += new System.EventHandler(this.txtContentType_TextChanged);
+            // 
+            // txtContentID
+            // 
+            this.txtContentID.Location = new System.Drawing.Point(173, 256);
+            this.txtContentID.Name = "txtContentID";
+            this.txtContentID.Size = new System.Drawing.Size(464, 22);
+            this.txtContentID.TabIndex = 9;
+            // 
+            // txtCategoryID
+            // 
+            this.txtCategoryID.Location = new System.Drawing.Point(174, 296);
+            this.txtCategoryID.Name = "txtCategoryID";
+            this.txtCategoryID.Size = new System.Drawing.Size(463, 22);
+            this.txtCategoryID.TabIndex = 10;
+            this.txtCategoryID.TextChanged += new System.EventHandler(this.txtCategoryID_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(44, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "File Name";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "File Path";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Extension";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "File Size";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(44, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 23);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Content Type";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(44, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 23);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Content ID";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.Control;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(45, 295);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 23);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Category ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(643, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 35);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(871, 632);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCategoryID);
+            this.Controls.Add(this.txtContentID);
+            this.Controls.Add(this.txtContentType);
+            this.Controls.Add(this.txtFileSize);
+            this.Controls.Add(this.txtExtension);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dgvDocuments);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBrowse);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "Form1";
+            this.Text = "File Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Label label7;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label FileName;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.DataGridView dgvDocuments;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.TextBox txtExtension;
+        private System.Windows.Forms.TextBox txtFileSize;
+        private System.Windows.Forms.TextBox txtContentType;
+        private System.Windows.Forms.TextBox txtContentID;
+        private System.Windows.Forms.TextBox txtCategoryID;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convert_To_Binary_Form
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Your_Database_Connection_String";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if(dlg.ShowDialog()==DialogResult.OK)
+            {
+                txtFileName.Text = Path.GetFileName(dlg.FileName);
+                txtFilePath.Text = dlg.FileName;
+                txtExtension.Text = Path.GetExtension(dlg.FileName);
+                txtFileSize.Text = new FileInfo(dlg.FileName).Length.ToString();
+            }
+            
+        }
+
+        private void dgvDocuments_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var selectedRow = dgvDocuments.SelectedRows;
+            foreach (var row  in selectedRow)
+            {
+                int id = (int) ((DataGridViewRow)row).Cells[0].Value;
+                OpenFile(id);
+            }
+        }
+
+        private void OpenFile(int id)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                
+                string query =
+                    "SELECT FileName,DataFile,Extension FROM ContentData WHERE DataID=@id";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.Add("@id", SqlDbType.Int).Value=id;
+                 
+                connection.Open();
+                var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    var name = reader["FileName"].ToString();
+                    var data = (byte[])reader["DataFile"];
+                    var extn = reader["Extension"].ToString();
+                    var newFileName = name.Replace(extn, DateTime.Now.ToString("ddMMyyyyhhmmss")) + extn;
+                    File.WriteAllBytes(newFileName,data);
+                    System.Diagnostics.Process.Start(newFileName);
+                }
+
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtFileName.Text)&& !string.IsNullOrEmpty(txtFilePath.Text)
+                &&!string.IsNullOrEmpty(txtExtension.Text)&& !string.IsNullOrEmpty(txtFileSize.Text)&&
+                !string.IsNullOrEmpty(txtContentType.Text)&& !string.IsNullOrEmpty(txtContentID.Text)&&
+                !string.IsNullOrEmpty(txtCategoryID.Text))
+            {
+                using (Stream stream = File.OpenRead(txtFilePath.Text))
+                {
+                    byte[] fileData = new byte[stream.Length];
+                    stream.Read(fileData, 0, fileData.Length);
+                    using (SqlConnection connection = GetConnection())
+                    {
+                        SqlCommand command = new SqlCommand("INSERT INTO ContentData (FileName,DataFile,FilePath,Extension,FileSize,ContentType,ContentID,CategoryID)" +
+                                                            "VALUES(@FileName,@DataFile,@FilePath,@Extension,@FileSize,@ContentType,@ContentID,@CategoryID)", connection);
+                        command.Parameters.Add("@FileName", SqlDbType.VarChar, 100).Value = txtFileName.Text;
+                        command.Parameters.Add("Datafile", SqlDbType.VarBinary, -1).Value = fileData;
+                        command.Parameters.Add("@FilePath", SqlDbType.VarChar, 255).Value = txtFilePath.Text;
+                        command.Parameters.Add("@Extension", SqlDbType.VarChar, 20).Value = txtExtension.Text;
+                        command.Parameters.Add("@FileSize", SqlDbType.VarChar, 50).Value = txtFileSize.Text;
+                        command.Parameters.Add("@ContentType", SqlDbType.VarChar, 50).Value = txtContentType.Text;
+                        command.Parameters.Add("@ContentID", SqlDbType.Int).Value = int.Parse(txtContentID.Text);
+                        command.Parameters.Add("@CategoryID", SqlDbType.Int).Value = int.Parse(txtCategoryID.Text);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+
+                        MessageBox.Show("Data saved successfully!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all the fields.");
+            }
+            
+
+        }
+     
+
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(@"Data Source=MADDY;Database=Content_Management_System;Integrated Security=true;");
+        }
+
+        private void txtFilePath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtContentType_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCategoryID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection connection=GetConnection())
+            {
+                string query = "SELECT DataID,FileName,FilePath,Extension,FileSize,ContentType,ContentID,CategoryID FROM ContentData";
+                SqlDataAdapter adapter = new SqlDataAdapter(query,connection);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    dgvDocuments.DataSource = dt;
+                }
+
+            }
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
